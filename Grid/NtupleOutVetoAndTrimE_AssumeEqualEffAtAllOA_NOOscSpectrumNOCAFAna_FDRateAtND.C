1b95a10cff8b6c443bdbd7c1893716b68ee0bdf9
mhanrahan27 <hanra063@umn.edu>
2026-06-11 15:09:54 -0500

    Update NtupleOutVetoAndTrimE_AssumeEqualEffAtAllOA_NOOscSpectrumNOCAFAna_FDRateAtND.C

--- Grid/NtupleOutVetoAndTrimE_AssumeEqualEffAtAllOA_NOOscSpectrumNOCAFAna_FDRateAtND.C
+++ Grid/NtupleOutVetoAndTrimE_AssumeEqualEffAtAllOA_NOOscSpectrumNOCAFAna_FDRateAtND.C
@@ -541,9 +541,9 @@ void ProcessFile(TFile *fHad, TFile *fMu){
     {
       cout<<" i_iwritten: "<<i_iwritten<<" weight CAF like: "<<weightCAFLike[i_iwritten]<<endl;
       //print weights before for loop that is breaking
-      cout <<" first for loop CoefficientsAtOAPos: "<<CoefficientsAtOAPos<< endl;
-      cout <<" first for loop WeightEventsAtOaPos: "<<WeightEventsAtOaPos << endl;
-      cout <<" first for loop weightCAFLike[i_iwritten]: "<<weightCAFLike[i_iwritten] << endl;
+      cout<<" first for loop, CoefficientsAtOAPos: "<<CoefficientsAtOAPos<<endl;
+      cout<<" first for loop, WeightEventsAtOaPos: "<<WeightEventsAtOaPos<<endl;
+      cout<<" first for loop, weightCAFLike[i_iwritten]: "<<weightCAFLike[i_iwritten]<<endl;
 
       if(TotalLeptonMom[i_iwritten] > 20) {
         cout<<" Emu > 20 GeV, Emu = "<<TotalLeptonMom[i_iwritten]<<" not interested in so high energies, skip event " <<endl;
